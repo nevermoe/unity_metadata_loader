220b3409d3d429a2e42a3bddff73ba088b4a8a18
nevermoe@users.noreply.github.com <nevermoe@users.noreply.github.com>
2016-08-20 01:02:21 +0900

    modify unity_loader.py

--- unity_loader.py
+++ unity_loader.py
@@ -2,6 +2,34 @@ import idaapi
 import os
 import re
 
+def ParseMethod():
+    
+    ea = ScreenEA();
+    
+    path = os.getcwd()
+    os.system(path+'/unity_decoder.exe')
+    file = open('./method_name.txt')
+    
+    str_count = file.readline()
+    i = 0;
+
+    for line in file:
+        line = line.replace(' ', '_')
+        line = re.sub(r'[^a-zA-Z0-9_$]', '', line)
+        
+        print line
+        if len(line) > 0 and line[0].isdigit():
+            line = 's' + line
+        
+        #addr = idc.Qword(ea)
+        #idc.MakeName(addr, str(line))
+        idc.MakeComm(ea, str(line))
+        
+        #ea = ea + 8
+        ea = ea + 4
+    
+    file.close()
+    
 
 def ParseString():
 
@@ -30,10 +58,11 @@ def ParseString():
     
 
  
- 
-# IDA binds hotkeys to IDC functions so a trampoline IDC function
 # must be created
 idaapi.CompileLine('static ParseString() { RunPythonStatement("ParseString()"); }')
+idaapi.CompileLine('static ParseMethod() { RunPythonStatement("ParseMethod()"); }')
+
 # Add the hotkey
 AddHotkey("Alt-F8", 'ParseString')
+AddHotkey("Alt-F9", 'ParseMethod')
 
