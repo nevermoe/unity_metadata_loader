6d51f8f616ec5120a8df31cd7aa5cd33fc51a1be
nevermoe@users.noreply.github.com <nevermoe@users.noreply.github.com>
2016-08-22 10:58:14 +0900

    more robust

--- unity_loader.py
+++ unity_loader.py
@@ -1,6 +1,7 @@
 import idaapi
 import os
 import re
+import random
 
 def ParseMethod():
     
@@ -20,13 +21,21 @@ def ParseMethod():
         if bits == 32: 
             addr = idc.Dword(ea)
             ret = idc.MakeNameEx(addr, str(new_line), SN_NOWARN)
-            idc.MakeComm(ea, str(line))
+            while ret == 0: # failed
+                new_line_rand = new_line + '_' + str(random.randint(0, 99999))
+                ret = idc.MakeNameEx(addr, str(new_line_rand), SN_NOWARN)
+                idc.MakeComm(ea, str(line))
+                
             ea = ea + 4
             
         elif bits == 64:
             addr = idc.Qword(ea)
             ret = idc.MakeNameEx(addr, str(new_line), SN_NOWARN)
-            idc.MakeComm(ea, str(line))
+            while ret == 0: # failed
+                new_line_rand = new_line + '_' + str(random.randint(0, 99999))
+                ret = idc.MakeNameEx(addr, str(new_line_rand), SN_NOWARN)
+                idc.MakeComm(ea, str(line))
+                
             ea = ea + 8
         
     
@@ -49,16 +58,25 @@ def ParseString():
         new_line = re.sub(r'[^a-zA-Z0-9_]', '_', line)
         
         if len(new_line) > 0 and new_line[0].isdigit():
-            new_line = 's_' + new_line
+            new_line = 'StringLiteral_' + new_line
         
         if bits == 64:
             addr = idc.Qword(ea)
             ret = idc.MakeNameEx(addr, str(new_line), SN_NOWARN)
+            while ret == 0: # failed
+                new_line_rand = 'StringLiteral' + new_line + '_' + str(random.randint(0, 99999))
+                ret = idc.MakeNameEx(addr, str(new_line_rand), SN_NOWARN)
+                
             idc.MakeComm(ea, str(line))
             ea = ea - 8
+            
         elif bits == 32:
             addr = idc.Dword(ea)
             ret = idc.MakeNameEx(addr, str(new_line), SN_NOWARN)
+            while ret == 0: # failed
+                new_line_rand = 'StringLiteral_' + new_line + '_' + str(random.randint(0, 99999))
+                ret = idc.MakeNameEx(addr, str(new_line_rand), SN_NOWARN)
+                
             idc.MakeComm(ea, str(line))
             ea = ea - 4
     
