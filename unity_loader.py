4f91b43636866e93f836bdcaf4245e7294e827b9
nevermoe <n3v3rm03@gmail.com>
2018-06-23 06:00:27 +0900

    rename unity_loader.py

--- unity_loader.py
+++ unity_loader.py
@@ -1,217 +1,21 @@
-import idaapi
 import os, sys
 import re
 import random
+import idaapi
+import idc
+import idautils
 
 
-def IncreaseAddr(addr):
-    if BITS == 64:
-        return addr+8
-    elif BITS == 32:
-        return addr+4
-        
-        
-def DecreaseAddr(addr):
-    if BITS == 64:
-        return addr-8
-    elif BITS == 32:
-        return addr-4
-        
-def GetVarFromAddr(addr):
-    if BITS == 64:
-        return idc.Qword(addr)
-    elif BITS == 32:
-        return idc.Dword(addr)
-        
-def GetMethodFromAddr(addr):
-    return GetVarFromAddr(addr) & 0xFFFFFFFFFE
+INFO = idaapi.get_inf_structure()
 
-def LoadMethods(ea = None):
-    
-    if ea is None:
-        ea = ScreenEA();
-    
-    path = os.getcwd()
-    os.system(path+'/unity_decoder.exe')
-    file = open('./method_name.txt')
-    
-    str_count = file.readline()
-    i = 0;
+if INFO.is_64bit():
+    BITS = 64
+elif INFO.is_32bit():
+    BITS = 32
+else:
+    BITS = 16
 
-    for line in file:
-        line = line.strip(' ').replace('\r', '').replace('\n', '')
-        new_line = re.sub(r'[^a-zA-Z0-9_$]', '_', line)
-        
-        i = 0;
-        addr = GetMethodFromAddr(ea)
-        ret = idc.MakeNameEx(addr, str(new_line), SN_NOWARN)
-        while ret == 0 and i < 5: # failed
-            new_line_rand = new_line + '_' + str(random.randint(0, 99999))
-            ret = idc.MakeNameEx(addr, str(new_line_rand), SN_NOWARN)
-            idc.MakeComm(ea, str(line))
-            i = i + 1
-            
-        ea = IncreaseAddr(ea)
 
-        
-    
-    file.close()
-    
-
-def LoadStrings(ea = None):
-
-    if ea is None:
-        ea = ScreenEA();
-    
-    path = os.getcwd()
-    os.system(path+'/unity_decoder.exe')
-    file = open('./string_literal.txt')
-    
-    str_count = file.readline()
-
-    for line in file:
-        line = line.strip(' ').replace('\r', '').replace('\n', '')
-        new_line = re.sub(r'[^a-zA-Z0-9_]', '_', line)
-        new_line = 'StringLiteral_' + new_line
-        
-       
-        i = 0;
-        addr = GetVarFromAddr(ea)
-        ret = idc.MakeNameEx(addr, str(new_line), SN_NOWARN)
-        while ret == 0 and i < 5: # failed
-            new_line_rand = new_line + '_' + str(random.randint(0, 99999))
-            ret = idc.MakeNameEx(addr, str(new_line_rand), SN_NOWARN)
-            i = i + 1
-             
-        idc.MakeComm(ea, str(line))
-        ea = DecreaseAddr(ea)
-            
-
-    
-    file.close()
-
-    
-def IsCode(addr):
-    return idc.isCode(idc.GetFlags(addr))
-    
-def IsData(addr):
-    return idc.isData(idc.GetFlags(addr))
-    
-def IsInSegment(addr, segName):
-    return segName == idc.SegName(addr)
-    
-def IsSubFollowing(addr):
-    i = 0
-    while i < 20:
-        pAddr = GetMethodFromAddr(addr)
-        if not IsCode(pAddr):
-            return False;
-        
-        addr = idc.NextHead(addr)
-        i = i + 1
-        
-    return True
-    
-    
-def IsStringPrecedingiOS(addr):
-    i = 0
-    while i < 10:
-        pAddr = GetVarFromAddr(addr)
-        if not IsInSegment(pAddr, '__common'):
-            return False;
-        
-        addr = DecreaseAddr(addr)
-        i = i + 1
-        
-    return True
-    
-def IsStringFollowingAndroid(addr):
-    i = 0
-    while i < 10:
-        if not IsData(addr):
-            return False;
-        
-        addr = idc.NextHead(addr)
-        i = i + 1
-        
-    return True
-
-    
-def FindStringsEndAndroid(addr):
-    while True:
-        addr = IncreaseAddr(addr)
-        if not IsData(addr):
-            addr = idc.PrevHead(idc.PrevHead(addr))
-            return addr;
-    
-def AutoLoadAndroid():
-    constSegList = [];
-    seg = idc.FirstSeg()
-    while seg != idc.BADADDR:
-        seg = idc.NextSeg(seg)
-        segName = idc.SegName(seg)
-        if segName == ".data.rel.ro":
-            constSegList.append(seg)
-    
-    print "Locating Methods and Strings..."
-    for addr in constSegList:
-        while idc.SegName(addr) == '.data.rel.ro':
-            if IsSubFollowing(addr) and IsStringFollowingAndroid(GetVarFromAddr(idc.PrevHead(addr))):
-                print "Methods Start At: %x" % (addr)
-                idc.Jump(addr)
-                break;
-            
-            addr = idc.NextHead(addr)
-    
-    print "Loading Symbols..."
-    LoadMethods(addr)
-    
-    addr = FindStringsEndAndroid(GetVarFromAddr(idc.PrevHead(addr)))
-    print "Strings End At %x" % (addr)
-    LoadStrings(addr)
-    
-    print "Done."
-    
-def AutoLoadiOS():
-    constSegList = [];
-    seg = idc.FirstSeg()
-    while seg != idc.BADADDR:
-        seg = idc.NextSeg(seg)
-        segName = idc.SegName(seg)
-        if segName == "__const":
-            constSegList.append(seg)
-    
-    print "Locating Methods and Strings..."
-    for addr in constSegList:
-        while idc.SegName(addr) == '__const':
-            if IsSubFollowing(addr) and IsStringPrecedingiOS(DecreaseAddr(addr)):
-                print "Methods Start At: %x" % (addr)
-                idc.Jump(addr)
-                break;
-            
-            addr = idc.NextHead(addr)
-    
-    print "Loading Symbols..."
-    LoadMethods(addr)
-    LoadStrings(idc.PrevHead(addr))
-    
-    print "Done."
-    
-    
-def AutoLoad():
-
-    if FILE_TYPE == "f_MACHO":
-        AutoLoadiOS()
-    elif FILE_TYPE == "f_ELF":
-        AutoLoadAndroid()
-    else:
-        print "AutoLoad is not supported for your platform. Please try LocationHelper."
-    
-    
-def LocationHelper():
-    return True;
-    
-    
 ENUM_FILE_TYPE =\
 [
   "f_EXE_old",            #// MS DOS EXE File
@@ -242,35 +46,185 @@ ENUM_FILE_TYPE =\
   "f_MACHO",              #// Max OS X
 ];
 
-info = idaapi.get_inf_structure()
-BITS = None
+
+FILE_TYPE = ENUM_FILE_TYPE[INFO.filetype]
 
 
-if info.is_64bit():
-    BITS = 64
-elif info.is_32bit():
-    BITS = 32
-else:
-    BITS = 16    
+def IncreaseAddr(addr):
+    if BITS == 64:
+        return addr+8
+    elif BITS == 32:
+        return addr+4
+        
+        
+def DecreaseAddr(addr):
+    if BITS == 64:
+        return addr-8
+    elif BITS == 32:
+        return addr-4
+        
+def GetVarFromAddr(addr):
+    if BITS == 64:
+        return idc.Qword(addr)
+    elif BITS == 32:
+        return idc.Dword(addr)
 
-FILE_TYPE = ENUM_FILE_TYPE[info.filetype]
-#print FILE_TYPE
- 
+def LocatePointerLists():
+    seg = idc.FirstSeg()
+    initArrayAddr = 0
+    while seg != idc.BADADDR:
+        seg = idc.NextSeg(seg)
+        segName = idc.SegName(seg)
+        if segName == ".init_array":
+            initArrayAddr = idc.SegStart(seg)
+            break
+    
+    # find Il2CppCodeRegistrationOffset from init_array
+    Il2CppCodeRegistrationOffset = initArrayAddr + 30 * (BITS / 8)
+    print "find Il2CppCodeRegistrationOffset %x" % Il2CppCodeRegistrationOffset
+
+    Il2CppCodeRegistrationCpp = GetVarFromAddr(Il2CppCodeRegistrationOffset)
+
+    print "Il2CppCodeRegistrationCpp: %x" % Il2CppCodeRegistrationCpp
+    idc.MakeName(Il2CppCodeRegistrationCpp, "Il2CppCodeRegistrationCpp")
+    
+    #Il2CppCodegenRegistration = 0
+    #for r in idautils.XrefsFrom(Il2CppCodeRegistrationAddr + 0x14, 0):
+    #    Il2CppCodegenRegistration = hex(r.to)
+        
+    #g_CodeRegistration = 0
+    #for r in idautils.XrefsFrom(Il2CppCodegenRegistration + 0x18, 0):
+    #    g_CodeRegistration = hex(r.to)
+    
+    opndValue = idc.GetOperandValue(Il2CppCodeRegistrationCpp + 0x8,1)
+    offset = GetVarFromAddr(opndValue)
+    
+    _GLOBAL_OFFSET_TABLE_ = idc.LocByName("_GLOBAL_OFFSET_TABLE_")
+    
+    
+    print "_GLOBAL_OFFSET_TABLE_ %x" % _GLOBAL_OFFSET_TABLE_
+    
+    
+    Il2CppCodegenRegistration = (_GLOBAL_OFFSET_TABLE_ + offset) & 0xFFFFFFFF
+    idc.MakeName(Il2CppCodegenRegistration, "Il2CppCodegenRegistration")
+    print "Il2CppCodegenRegistration %x" % Il2CppCodegenRegistration
+    
+    opndValue = idc.GetOperandValue(Il2CppCodegenRegistration + 0xC,1)
+    offset = GetVarFromAddr(opndValue)
+    g_CodeRegistration = (_GLOBAL_OFFSET_TABLE_ + offset) & 0xFFFFFFFF
+    idc.MakeName(g_CodeRegistration, "g_CodeRegistration")
+    print "g_CodeRegistration %x" % g_CodeRegistration
+    
+    g_MethodPointers = GetVarFromAddr(g_CodeRegistration + 0x4)
+    idc.MakeName(g_MethodPointers, "g_MethodPointers")
+    print "g_MethodPointers %x" % g_MethodPointers
+    
+    
+    opndValue = idc.GetOperandValue(Il2CppCodegenRegistration + 0x04,1)
+    offset = GetVarFromAddr(opndValue)
+    g_MetadataRegistration = GetVarFromAddr((_GLOBAL_OFFSET_TABLE_ + offset) & 0xFFFFFFFF)
+    idc.MakeName(g_MetadataRegistration, "g_MetadataRegistration")
+    print "g_MetadataRegistration %x" % g_MetadataRegistration
+    
+    g_MetadataUsages = GetVarFromAddr(g_MetadataRegistration + 0x3C)
+    idc.MakeName(g_MetadataUsages, "g_MetadataUsages")
+    print "g_MetadataUsages %x" % g_MetadataUsages
+    
+    return (g_MethodPointers, g_MetadataUsages)
+
+def GetMethodFromAddr(addr):
+    return GetVarFromAddr(addr) & 0xFFFFFFFE
+    
+    
+def LoadMethods(ea = None):
+    
+    if ea is None:
+        ea = ScreenEA();
+    
+    path = os.getcwd()
+    os.system(path+'/unity_decoder.exe')
+    file = open('./method_name.txt')
+    
+    str_count = file.readline()
+    i = 0;
+
+    for line in file:
+        line = line.strip(' ').replace('\r', '').replace('\n', '')
+        new_line = re.sub(r'[^a-zA-Z0-9_$]', '_', line)
+        
+        i = 0;
+        addr = GetMethodFromAddr(ea)
+        ret = idc.MakeNameEx(addr, str(new_line), SN_NOWARN)
+        while ret == 0 and i < 5: # failed
+            new_line_rand = new_line + '_' + str(random.randint(0, 99999))
+            ret = idc.MakeNameEx(addr, str(new_line_rand), SN_NOWARN)
+            idc.MakeComm(ea, str(line))
+            i = i + 1
+            
+        ea = IncreaseAddr(ea)
+
+    file.close()
+    
+def LoadStringLiterals(ea = None):
+
+    if ea is None:
+        ea = ScreenEA();
+    
+    path = os.getcwd()
+    os.system(path+'/unity_decoder.exe')
+    file = open('./string_literal.txt')
+    
+    str_count = file.readline()
+    skip_count = file.readline()
+    ea += int(skip_count) * 0x4
+
+    for line in file:
+        line = line.strip(' ').replace('\r', '').replace('\n', '')
+        new_line = re.sub(r'[^a-zA-Z0-9_]', '_', line)
+        new_line = 'StringLiteral_' + new_line
+        
+       
+        i = 0;
+        addr = GetVarFromAddr(ea)
+        ret = idc.MakeNameEx(addr, str(new_line), SN_NOWARN)
+        #ret = idc.MakeNameEx(addr, "", SN_NOWARN)
+        while ret == 0 and i < 5: # failed
+            new_line_rand = new_line + '_' + str(random.randint(0, 99999))
+            ret = idc.MakeNameEx(addr, str(new_line_rand), SN_NOWARN)
+            i = i + 1
+             
+        idc.MakeComm(ea, str(line))
+        #idc.MakeComm(ea, "")
+        ea = IncreaseAddr(ea)
+    
+    file.close()
+
+
+def AutoLoadAndroid():
+    (g_MethodPointers,g_MetadataUsages) = LocatePointerLists()
+    LoadMethods(g_MethodPointers)
+    LoadStringLiterals(g_MetadataUsages)
+
+def AutoLoad():
+
+    if FILE_TYPE == "f_ELF":
+        AutoLoadAndroid()
+    else:
+        print "AutoLoad is not supported for your platform. Please try LocationHelper."
+
 # must be created
-idaapi.CompileLine('static LoadStrings() { RunPythonStatement("LoadStrings()"); }')
+idaapi.CompileLine('static LoadStringLiterals() { RunPythonStatement("LoadStringLiterals()"); }')
 idaapi.CompileLine('static LoadMethods() { RunPythonStatement("LoadMethods()"); }')
 idaapi.CompileLine('static AutoLoad() { RunPythonStatement("AutoLoad()"); }')
-idaapi.CompileLine('static LocationHelper() { RunPythonStatement("LocationHelper()"); }')
+idaapi.CompileLine('static LocatePointerLists() { RunPythonStatement("LocatePointerLists()"); }')
 
 # Add the hotkey
-AddHotkey("Ctrl-Alt-S", 'LoadStrings')
+AddHotkey("Ctrl-Alt-S", 'LoadStringLiterals')
 AddHotkey("Ctrl-Alt-M", 'LoadMethods')
 AddHotkey("Ctrl-Alt-A", 'AutoLoad')
-AddHotkey("Ctrl-Alt-L", 'LocationHelper')
+AddHotkey("Ctrl-Alt-L", 'LocatePointerLists')
 
-print "Ctrl-Alt-S : Load String Literals"
-print "Ctrl-Alt-M : Load Methods"
-print "Ctrl-Alt-A : Automatically Load Everything (iOS and Android Only)"
-print "Ctrl-Alt-L : Locate Methods (TODO)"
-
-
+print "Ctrl-Alt-A : Automatically Load Everything"
+print "Ctrl-Alt-S : (Advanced) Load String Literals"
+print "Ctrl-Alt-M : (Advanced) Load Methods"
+print "Ctrl-Alt-L : Locate Method Pointers and Stringliteral Pointers"
